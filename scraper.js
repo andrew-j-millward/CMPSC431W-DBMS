1a65cd2d5f984c51b9bd33745ecc98fcbae65df2
D.S <dsuprunsw@gmail.com>
2020-11-25 18:04:50 -0500

    Fix name parsing.

--- scraper.js
+++ scraper.js
@@ -4,14 +4,12 @@ const got = require('got');
 
 const base_url = 'https://wiki.wargaming.net/en/'
 const tanks_url = base_url + 'Medium_Tanks'
-
-const get_name_from = (responce) => {
-
-}
 got(tanks_url).then(response => {
    const $ = cheerio.load(response.body);
    let all_tanks = $("div[class |= 'b-description-list_item']").children();
-   console.log(all_tanks[0].children[4].data);
+   for (let i = 0; i < Object.keys(all_tanks).length; i++) {
+      console.log(all_tanks[i].children[3].children[1].children[0].data);
+   }
 }).catch(err => {
    console.log(err);
 });
