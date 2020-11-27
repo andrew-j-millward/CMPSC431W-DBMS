0672a62b840d6281a55802b9caff121272f6e008
D.S <dsuprunsw@gmail.com>
2020-11-27 17:37:27 -0500

    Use promise to return value from a callback.

--- scraper.js
+++ scraper.js
@@ -15,14 +15,26 @@ const countries_id = {
    "Czechoslovakia": 10,
    "Italy": 11
 };
-const base_url = 'https://wiki.wargaming.net/en/'
-const tanks_url = base_url + 'Medium_Tanks'
-got(tanks_url).then(response => {
-   const $ = cheerio.load(response.body);
-   let all_tanks = $("div[class |= 'b-description-list_item']").children();
-   for (let i = 0; i < Object.keys(all_tanks).length; i++) {
-      console.log(all_tanks[i].children[3].children[1].children[0].data);
-   }
-}).catch(err => {
-   console.log(err);
+const base_url = 'https://wiki.wargaming.net/en/';
+const role_url = 'Heavy_Tanks';
+let retrieve_list = new Promise((resolve, reject) => {
+   let tanks_url = base_url + role_url;
+   let tanks_list = [];
+   got(tanks_url).then(response => {
+      const $ = cheerio.load(response.body);
+      let all_tanks = $("div[class |= 'b-description-list_item']").children();
+      for (let i = 0; i < Object.keys(all_tanks).length && 
+            all_tanks[i] != undefined; i++) {
+         const tank_name = all_tanks[i].children[3].children[1].children[0].data;
+         tanks_list.push(tank_name + ',3');
+         resolve(tanks_list);
+      }
+   }).catch(err => {
+      console.log(err);
+      reject('couldn\'t access server');
+   });
 });
+
+retrieve_list.then((data) => {
+   console.log(data);
+})
